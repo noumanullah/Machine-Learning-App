82d93360f5eaa59b58134139a244fc92b5362d3b
noumanullah <fastcoderworks@gmail.com>
2021-01-02 17:56:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
-
+var=
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
