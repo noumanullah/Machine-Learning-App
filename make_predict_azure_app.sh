af004bd30dbf056c0a13754f7da0cd42b71a1e71
noumanullah <fastcoderworks@gmail.com>
2021-01-02 17:40:50 +0500

    Update make_predict_azure_app.sh
    updated url

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flaskmlapp.azurewebsites.net:$PORT/predict 
+     -X POST https://fmlapp.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
